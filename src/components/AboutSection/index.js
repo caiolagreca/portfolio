ac5170ba31fa3eba3888f6a0a09183b82a211f52
Caio Lagreca <caiomiranda701@hotmail.com>
2022-08-31 12:26:22 -0300

    alteração do sobre mim

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -28,7 +28,7 @@ function AboutSection() {
         <AboutWrapper>
           <Column1>
             <TextWrapper data-aos="fade-right">
-            Olá! Sou Desenvolvedor Web Front-end, no qual atuo como Freelancer desenvolvendo projetos em ReactJS e Javascript, e estou em busca de minha primeira oportunidade. Também graduado em Engenharia Civil, e com grande experiência no mercado financeiro trabalhando em uma fintech. Anteriormente fui proprietário do site Criativa TecHouse, no qual importávamos produtos de tecnologia, e eu era responsável por toda a infra, manutenção do site, e comunicação com o cliente. Através dessas experiências desenvolvi skills de extrema relevância para atuar no mercado, com uma boa comunicação, facilidade para trabalhar em equipe, inteligência emocional, organização, planejamento e gestão de tempo. Além disso, falo inglês fluentemente, no qual estudo e pratico há mais de 15 anos.
+            Olá! Sou Desenvolvedor Web Front-end, no qual atuo como Freelancer desenvolvendo projetos em ReactJS e Javascript, e estou em busca de minha primeira oportunidade. Também graduado em Engenharia Civil, e com grande experiência no mercado financeiro trabalhando em uma fintech. Anteriormente fui proprietário de um site de importação de produtos de tecnologia, sendo responsável por toda a infra, manutenção do site, e comunicação com o cliente. Através dessas experiências desenvolvi skills de extrema relevância para atuar no mercado, com uma boa comunicação, facilidade para trabalhar em equipe, inteligência emocional, organização, planejamento e gestão de tempo. Além disso, falo inglês fluentemente, no qual estudo e pratico há mais de 15 anos.
             </TextWrapper>
           </Column1>
           <Column2>
